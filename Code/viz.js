80022ba3773a205934b5a99da785fde89443746f
Sahil Jain <jsahil95@gmail.com>
2018-04-25 09:06:23 -0700

    Update viz.js

--- Code/viz.js
+++ Code/viz.js
@@ -1,3 +1,7 @@
+<script src="http://d3js.org/d3.v4.min.js"></script>
+<script src="http://d3js.org/queue.v1.min.js"></script>
+<script src="http://d3js.org/topojson.v1.min.js"></script>
+
 var format = d3.format(",");
 
 // Set tooltips
